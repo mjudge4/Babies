446e3379c1185773a750cb1ae6a24817cc0bc2d4
mjudge4 <mjudge4@gmail.com>
2018-02-03 14:59:45 +0000

    Added login session NW

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import session as login_session
+import random, string
+
+# @reference http://https://classroom.udacity.com/courses/ud330/lessons/3967218625/concepts/39636486150923
+#Create a state token for forgery prevention and store in session for later validation
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
