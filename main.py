1826f4bb133adf5e0d7245511874e0456bafb231
mjudge4 <mjudge4@gmail.com>
2018-02-04 17:36:30 +0000

    GConnect code added - Needs Testing

--- main.py
+++ main.py
@@ -1,20 +1,109 @@
-from flask import Flask
+from flask import Flask, render_template, flash
+
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 from flask import session as login_session
 import random
 import string
 
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
 app = Flask(__name__)
 
 # @reference http://https://classroom.udacity.com/courses/ud330/lessons/3967218625/concepts/39636486150923
-#Create a state token for forgery prevention and store in session for later validation
+
+# Validate the state token
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Acquire authorisation code
+    code = request.data
+
+
+    try:
+        oauth_flow = flow_from_clientsecrets(client_secrets.json, scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorisation code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    #Check token is valid
+    access_token = credentials.access_token
+    url = ('')
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # Abort if there was an error in access token info
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Confirm access token is for the correct user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps("User ID does not match Token ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Confirm the access token is correct for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps("Token ID does not match to application"), 401)
+        print "Token ID does not match to application"
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to see if the user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('User is already logged in'), 200)
+        response.headers['Content-Type'] = 'application/json'
+
+    # Store access token in the session
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get the user's info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    # Store user data to  create a response
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    #Render Login Template return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 @app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
